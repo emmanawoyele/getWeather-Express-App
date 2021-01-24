df6056bc8ca3ba7ed61f5631d56214d4b9ac14f9
Manny <emmanawoyele@gmail.com>
2021-01-24 15:51:12 +0000

    index.js and index.hbs updated

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,8 @@ var Temp_2 = document.querySelector("#message2")
 var Temp_3 = document.querySelector("#message3")
 var Temp_4 = document.querySelector("#message4")
 var Temp_5 = document.querySelector("#message5")
+var Temp_6 = document.querySelector("#message6")
+var Temp_7 = document.querySelector("#message7")
 console.log(Temp_1)
 weatherForm.addEventListener('submit',function(e){
 e.preventDefault()
@@ -27,7 +29,10 @@ response.json().then((data)=>{
     {Temp_3.textContent="lat:"+data.latitude}
     {Temp_4.textContent="lon:" +data.longitude}
     {Temp_5.textContent="LocalTime:"+data.timeZone}
+    {Temp_6.textContent="Pressure:"+data.Pressure}
+    {Temp_7.textContent="wind direction:"+data.wind_dir}
     console.log(data)
+
     // var weatherResponse =document.createElement('ul')
     // var list = document.createElement('li')
     // var ult = weatherResponse.append(list)
